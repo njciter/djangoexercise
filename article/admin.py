03df068bbed6852f6705021c9ddd7c8222ab33d5
Yuanzhen Zhao <njciter@162.com>
2016-09-11 21:01:16 +0800

    Django exercise for the website2

---
+++ article/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Article
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ("block","title","content","status")
+
+admin.site.register(Article,ArticleAdmin)
--- block/admin.py
+++ block/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import Block
 
 class BlockAdmin(admin.ModelAdmin):
-    list_display = ("name","desc","manager_name")
+    list_display = ("name","desc","manager_name","status")
 
 
 admin.site.register(Block,BlockAdmin)
